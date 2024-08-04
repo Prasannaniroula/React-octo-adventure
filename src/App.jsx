e0634cac9b0953dd960ae98e1c66091f4560b221
Prasannaniroula <Prasannaniroula5@gmail.com>
2024-08-04 19:54:19 +0545

    except copy button everything is completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,22 @@ function App() {
   const [password, setPassword]=useState("")
   const passwordgenerator =useCallback(()=>{
    let alpha= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-   let symbol ="~!@#%^&*()_+{}[]"
-   let number = "1234567890"
    let pass=""
-   if(numberAllow) alpha+=number
-   if(symbolAllow) alpha+=symbol
-   for(i=0;i<=alpha.length;i++){
+   if(numberAllow) alpha+= "1234567890"
+   if(symbolAllow) alpha+="~!@#%^&*()_+{}[]"
+   for(let i=0;i<=length;i++){
     let char = Math.floor(Math.random()*alpha.length+1)
-    pass =pass+char
+    pass = pass+alpha.charAt(char)
    }
-   console.log(pass);
+   console.log(pass)
+   setPassword(pass)
 
   },[length,numberAllow,symbolAllow,setPassword])
+
+  useEffect(()=>{
+    passwordgenerator()
+  },[length,numberAllow,symbolAllow,setPassword])
+
 
   return (
     <>
@@ -34,13 +38,21 @@ function App() {
       </div>
      <div className='flex justify-center items-center '>
      
-      <input className='h-16 w-1/3 text-3xl text-center text-black bg-white mt-10 rounded-lg' type='text' placeholder='Your password here' readOnly></input>
+      <input className='h-16 w-2/3 text-3xl text-center text-black bg-white mt-10 rounded-l-lg' type='text' placeholder='Your password here' value={password} readOnly></input>
+      <button className='bg-yellow-100 w-28 h-16 mt-10 text-3xl rounded-r-lg'>Copy</button>
      </div>
      <div>
       <div className='flex items-center justify-center gap-16 mt-5 text-white' >
-     <input className='flex items-center justify-center' id='range' type='range' min={8} max={30}/>
-     <p><input id='check' type='checkbox'/> Number</p>
-     <p><input id='check' type='checkbox'/> Symbol</p>
+     <input className='flex items-center justify-center' id='range' type='range' min={8} max={30}
+     onChange={(e)=>{setLength(e.target.value)}}
+     value={0}/>
+     <p>Length:{length}</p>
+     <p><input id='check' type='checkbox'
+     onChange={()=>{setNumber((prev=>!prev))}}
+     /> Number</p>
+     <p><input id='check' type='checkbox'
+     onChange={()=>{setSymbol ((prev=>!prev))}}
+     /> Symbol</p>
      </div>
      </div>
      
